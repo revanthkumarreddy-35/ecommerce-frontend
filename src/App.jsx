3fefe77d825ddf92dd6db27f78184f00896da3e2
revanthkumarreddy-35 <revanth12428@gmail.com>
2025-08-04 10:48:15 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/ecommerce-frontend/">
+    <Router basename="/ecommerce/">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
